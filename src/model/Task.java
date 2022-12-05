4be1c902fb9bd0d21923f3f6bae631272591cf24
Anatoly Mishin <mishin.anatoly@yandex.ru>
2022-12-05 15:28:58 +0300

    feat: Add Task and Status

---
+++ src/model/Task.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Task {
+    private long id;
+    private String name;
+    private String description;
+    private Status status;
+
+    public Task(long id, String name, String description, Status status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
